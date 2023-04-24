277741e4f2e0035d8abfcf8eeee9bf2162ae2cfb
Jeremy Hurren <lordjeb@lordjeb.com>
2023-04-24 11:47:07 -0600

    Add project frameworks

---
+++ src/app/app.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
---
+++ src/app/pch.h
@@ -0,0 +1,8 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <SFML\Graphics.hpp>
+
+#include <Windows.h>
+
+#endif //PCH_H
